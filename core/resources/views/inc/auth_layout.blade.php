b74a24fb78b115de7da001f6e84aba9e287fb484
AJANG JOSHUA NUHU <joshuanuhuajang@gmail.com>
2021-01-13 00:58:04 +0000

    New Favicons

--- core/resources/views/inc/auth_layout.blade.php
+++ core/resources/views/inc/auth_layout.blade.php
@@ -5,6 +5,13 @@
     <title>{{$settings->site_title}} - {{$settings->site_descr}}</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <link rel="icon" href="/img/{{$settings->site_logo}}" type="image/x-icon"/>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
 
     <!-- Fonts and icons -->
     <script src="/atlantis/js/plugin/webfont/webfont.min.js"></script>
