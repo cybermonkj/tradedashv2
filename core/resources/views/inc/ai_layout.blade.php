eb3d6811ef2974c169ea88970694969ed434235b
AJANG JOSHUA NUHU <joshuanuhuajang@gmail.com>
2021-01-13 02:30:48 +0000

    added tawk.to

--- core/resources/views/inc/ai_layout.blade.php
+++ core/resources/views/inc/ai_layout.blade.php
@@ -54,6 +54,15 @@
 <!-- Footer End-->
 
 </div>
+<script>var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5fdc8ecbdf060f156a8e4c92/1epqorjr7';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();</script>
 <script src="/atlantis/js/core/popper.min.js"></script>
 <script src="/atlantis/js/core/bootstrap.min.js"></script>
 
