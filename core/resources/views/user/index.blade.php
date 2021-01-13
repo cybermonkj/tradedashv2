a91cd515057c2ce260c767e4460445373e2b9b44
AJANG JOSHUA NUHU <joshuanuhuajang@gmail.com>
2021-01-13 02:04:24 +0000

    Update

--- core/resources/views/user/index.blade.php
+++ core/resources/views/user/index.blade.php
@@ -12,7 +12,7 @@
 										<!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com" rel="noopener" target="_blank"><span class="blue-text">Our Traders are currently watching</span></a></div>
+  <div class="tradingview-widget-copyright"><a href="https://" rel="noopener" target="_blank"><span class="blue-text">Our Traders are currently watching</span></a></div>
   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
   {
   "symbols": [
