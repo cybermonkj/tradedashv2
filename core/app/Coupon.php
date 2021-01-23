c61acbcbaa2dfd39369218bf45e0fbda1d395362
AJANG JOSHUA NUHU <joshuanuhuajang@gmail.com>
2021-01-23 21:35:26 +0000

    Updates coupon model and fixed factory issue

--- core/app/Coupon.php
+++ core/app/Coupon.php
@@ -2,12 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
-    use HasFactory;
 
     protected $fillable = [
         'coupon_code',
