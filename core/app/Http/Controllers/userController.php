dfc24f530ba948f709f8478f14d9c10e608ad70e
Snapster02 <alabson.inc@gmail.com>
2021-01-25 14:30:17 +0100

    test

--- core/app/Http/Controllers/userController.php
+++ core/app/Http/Controllers/userController.php
@@ -1710,114 +1710,65 @@ class userController extends Controller
     }
 
   }
-  // Bank deposit Controller//
-public function bank_deposit(Request $req){
-  $user = Auth::User();
-  if(!empty($user))
-  {
-    if($req->input('amt') < env('MIN_DEPOSIT'))
-    {
-      return back()->With(['toast_msg' => 'x']);
-    }
-    try{
-      $st = site_settings::find(1);
-      $paymt = new deposits;
-      $paymt->user_id = $user->id;
-      $paymt->usn = $user->username;
-      $paymt->amount = $req->input('amt');
-      $paymt->currency = $st->currency;
-      $paymt->account_name = $req->input('account_name');
-      $paymt->account_no = $req->input('account_no');
-      $paymt->bank = "Bank";
-      $paymt->url =  "";
-      $paymt->status = 0;
-      $paymt->on_apr = 0;
-      $paymt->pop = "";
+// Bank deposit Controller//
+// public function bank_deposit(Request $req){
+//   $user = Auth::User();
+//   if(!empty($user))
+//   {
+//     if($req->input('amt') < env('MIN_DEPOSIT'))
+//     {
+//       return back()->With(['toast_msg' => 'x']);
+//     }
+//     try{
+//       $st = site_settings::find(1);
+//       $paymt = new deposits;
+//       $paymt->user_id = $user->id;
+//       $paymt->usn = $user->username;
+//       $paymt->amount = $req->input('amt');
+//       $paymt->currency = $st->currency;
+//       $paymt->account_name = $req->input('account_name');
+//       $paymt->account_no = $req->input('account_no');
+//       $paymt->bank = "Bank";
+//       $paymt->url =  "";
+//       $paymt->status = 0;
+//       $paymt->on_apr = 0;
+//       $paymt->pop = "";
 
-      $paymt->save();
+//       $paymt->save();
 
-      $maildata = ['email' => $user->email, 'username' => $user->username];
+//       $maildata = ['email' => $user->email, 'username' => $user->username];
 
-      Mail::send('mail.user_deposit_notification', ['md' => $maildata], function($msg) use ($maildata){
-          $msg->from(env('MAIL_USERNAME'), env('APP_NAME'));
-          $msg->to($maildata['email']);
-          $msg->subject('User Deposit Notification');
-      });
+//       Mail::send('mail.user_deposit_notification', ['md' => $maildata], function($msg) use ($maildata){
+//           $msg->from(env('MAIL_USERNAME'), env('APP_NAME'));
+//           $msg->to($maildata['email']);
+//           $msg->subject('User Deposit Notification');
+//       });
 
-      Mail::send('mail.admin_deposit_notification', ['md' => $maildata], function($msg) use ($maildata){
-          $msg->from(env('MAIL_USERNAME'), env('APP_NAME'));
-          $msg->to(env('SUPPORT_EMAIL'));
-          $msg->subject('User Deposit Notification');
-      });
+//       Mail::send('mail.admin_deposit_notification', ['md' => $maildata], function($msg) use ($maildata){
+//           $msg->from(env('MAIL_USERNAME'), env('APP_NAME'));
+//           $msg->to(env('SUPPORT_EMAIL'));
+//           $msg->subject('User Deposit Notification');
+//       });
 
-      return back()->With(['toast_msg' => 'Deposit Code saved! Please incase of System failure automatically processing. also submit details of deposit transaction to moderators to speed up funding your wallet via support chat or '.env('BANK_DEPOSIT_EMAIL'), 'toast_type' => 'suc']);
-    }
-    catch(\Exception $e)
-    {
-      return back()->With(['toast_msg' => 'Error saving your record. Please try again', 'toast_type' => 'err']);
-    }
-  }
-  else
-  {
-    return redirect('/login');
-  }
-}
+//       return back()->With(['toast_msg' => 'Deposit Code saved! Please incase of System failure automatically processing. also submit details of deposit transaction to moderators to speed up funding your wallet via support chat or '.env('BANK_DEPOSIT_EMAIL'), 'toast_type' => 'suc']);
+//     }
+//     catch(\Exception $e)
+//     {
+//       return back()->With(['toast_msg' => 'Error saving your record. Please try again', 'toast_type' => 'err']);
+//     }
+//   }
+//   else
+//   {
+//     return redirect('/login');
+//   }
+// }
  
 // Coupon controller
 
-public function coupon(Request $request)
-{
+public function coupon(Request $request) {
   $user = Auth::user();
 
   $input = Input::all();
-
-  // return redirect('/{username}/wallet')->back('mssg', $input);
-
-  // return redirect()->back()-> withInput();
-//    $id = Auth::id();
-
-   
-//    // Check if user is logged in
-//     if (Auth::check()) {
-
-//         // Check amount to deposited
-//        if ($request->input('amt') < env('MIN_DEPOSIT')) {
-//             // return back()->with([
-//             //    'toast_msg' => 'Amount must be greater or equal to '.env('MIN_DEPOSIT').' '.$this->st->currency, 
-//             //    'toast_type' => 'err'
-//             // ]);
-//             Session::flash('message', "Deposit code MIN");
-//             return redirect()->back();
-//        } elseif ($request->input('amt') > env('MAX_DEPOSIT')) {
-//             // return back()->with([
-//             //    'toast_mg' => 'Amount must be greater or equal to '.env('MIN_DEPOSIT').' '.$this->st->currency,
-//             //    'toast_type' => 'err'
-//             // ]);
-//             Session::flash('message', "Deposit code MAX");
-//             return redirect()->back();
-//        }
-
-
-//        // match inputed coupon_code with that on the db
-//        $coupon_col = DB::table('coupons')->where('coupon_code', $request->input('deposit_code'));
-
-//        if (!empty($coupon_col)) {
-//            if ($coupon_col->is_used == false) {
-//             //    return redirect('/deposit/validate');
-//                 Session::flash('message', "Desposit code unused!");
-//                 return redirect()->back();
-//            } else if ($coupon_col->is_used == true) {
-//                 Session::flash('message', "Deposit code has been used!");
-//                 return redirect()->back();
-//            }
-//        } else {
-//             Session::flash('message', "The deposit code you inputed is invalid!");
-//             return redirect()->back();
-//        }
-       
-
-
-//     }
 }
 
   public function view_tickets()
