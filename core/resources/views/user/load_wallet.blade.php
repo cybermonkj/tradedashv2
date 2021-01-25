7286903ca75e84e41bf40f7496b6ba67be54cc2d
Snapster02 <alabson.inc@gmail.com>
2021-01-25 13:40:18 +0100

    Updated cancel btn

--- core/resources/views/user/load_wallet.blade.php
+++ core/resources/views/user/load_wallet.blade.php
@@ -391,7 +391,7 @@
                             <div class="container">
                                 <div class="form-group">
                                     <input type="submit" value="submit" class="btn btn-info">
-                                    <a id="bank_deposit_cont_dets_close" href="javascript" class="collcc btn btn-danger">{{ __('Cancel') }}</a>
+                                    <a id="bank_deposit_cont_dets_close" class="collcc btn btn-danger">{{ __('Cancel') }}</a>
                                 </div>
                             </div>
 						
