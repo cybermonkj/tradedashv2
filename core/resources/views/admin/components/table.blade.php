fe3990b91b28352288f01227cdd6373034dce55a
Snapster02 <alabson.inc@gmail.com>
2021-01-26 11:50:35 +0100

    Updated table using grids and flex / responsive design

--- core/resources/views/admin/components/table.blade.php
+++ core/resources/views/admin/components/table.blade.php
@@ -8,27 +8,90 @@
     }
 
     .table__main {
+        width: 100%;
         
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        grid-template-rows: repeat(1, fr);
+        grid-template-areas: 
+            "header content"
+        ;
     }
-
-    .table__main {
-        
-    }
-
     .table__header {
         
     }
 
     .table__row--header {
-        
+        display: flex;
+        flex-direction: column;
     }
 
     .table__row--header th {
+        grid-area: header;
+        height: 4rem;
+        width: calc(100% / 6%);
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+
+        text-transform: capitalize;
+    }
+
+    .table__row--content {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .table__row--content td {
+        grid-area: content;
+        height: 4rem;
+        width: calc(100% / 6%);
         
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
     }
 
     .table__body {
+        width: 100%;
+    }
+
+    .table__data {
         
+    }
+
+    @media screen and (min-width: 768px) {
+        .table__main {
+            grid-template-columns: 1fr;
+            grid-template-rows: repeat(2, 1fr);
+            grid-template-areas: 
+                "header"
+                "content"
+            ;
+        }
+
+        .table__row--header {
+            flex-direction: row;
+        }
+
+        .table__row--header th {
+            justify-content: space-evenly;
+
+            width: 100%;
+            height: 4rem;
+        }
+
+        .table__row--content {
+            flex-direction: row;
+            width: 100%; 
+        }
+
+        .table__row--content td {
+            justify-content: center;
+            width: 100%;
+            height: 3rem;
+        }
     }
 </style>
 
@@ -48,17 +111,19 @@
         </thead>
 
         <tbody class="table__body">
-            <td class="table__data fw-normal">1</td>
-            <td class="table__data fw-normal">7Ur83Gy4</td>
-            <td class="table__data fw-normal">Sold</td>
-            <td class="table__data fw-normal">Date</td>
-            <td class="table__data fw-normal">Date</td>
-            <td class="table__data fw-normal">
-                <div class="gap-2 d-grid">
-                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                    <button type="button" class="btn btn-primary btn-sm">Update</button>
-                </div>
-            </td>
+            <tr class="table__row--content">
+                <td class="table__data fw-normal">1</td>
+                <td class="table__data fw-normal">7Ur83Gy4</td>
+                <td class="table__data fw-normal">Sold</td>
+                <td class="table__data fw-normal">Date</td>
+                <td class="table__data fw-normal">Date</td>
+                <td class="table__data fw-normal">
+                    <div class="gap-2 d-grid">
+                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                        <button type="button" class="btn btn-primary btn-sm">Update</button>
+                    </div>
+                </td>
+            </tr>
         </tbody>
     </table>
 </div>
--- core/resources/views/admin/coupon_code.blade.php
+++ core/resources/views/admin/coupon_code.blade.php
@@ -11,7 +11,7 @@
 							<h1 class="text-uppercase display-5">Coupon Codes</h1>
 						</div>
 
-						<div class="p-4 shadow card">
+						<div class="p-4 shadow card" style="width: 100% !important">
 							@include('admin.components.table')
 						</div>
 					</div>
