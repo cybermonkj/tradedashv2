980dc2e6828874fafcf0b6224ab46051974c941e
Snapster02 <alabson.inc@gmail.com>
2021-01-26 21:45:47 +0100

    altered UI

--- core/resources/views/admin/components/table.blade.php
+++ core/resources/views/admin/components/table.blade.php
@@ -63,7 +63,7 @@
         
     }
 
-    @media screen and (min-width: 768px) {
+    @media only screen and (max-width: 420px) {
         .table__main {
             grid-template-columns: 1fr;
             grid-template-rows: repeat(2, 1fr);
