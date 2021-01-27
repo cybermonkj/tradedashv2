2167b1b6781cc1145985ed3b9103fba451f9c589
AJANG JOSHUA NUHU <joshuanuhuajang@gmail.com>
2021-01-27 15:10:51 +0000

    Changes made

--- core/app/Http/Controllers/DepositController.php
+++ core/app/Http/Controllers/DepositController.php
@@ -59,8 +59,8 @@ class DepositController extends Controller
                                     $depositHist->deposit_id = ($user->id .'_'. $coupon->id .'_'. $bank->id);
                                     $depositHist->usn = ($user->firstname .' '. $user->lastname);
                                     $depositHist->amount = $request->input('amount');
-                                    $depositHist->acct_name = $bank->Account_name;
-                                    $depositHist->acct_no = $bank->Account_number;
+                                    $depositHist->acctount_name = $bank->Account_name;
+                                    $depositHist->acctount_no = $bank->Account_number;
                                     $depositHist->currency = DB::table('settings')->where('id', 1)->value('currency');
                                     $depositHist->bank = $bank->Bank_Name;
                                     $depositHist->transaction_type = null;
