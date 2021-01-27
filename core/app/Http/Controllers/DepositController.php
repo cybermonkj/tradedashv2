7fecf3a8e58116eb40ac621bfce4206c334dd164
Snapster02 <alabson.inc@gmail.com>
2021-01-27 08:13:33 +0100

    changes

--- core/app/Http/Controllers/DepositController.php
+++ core/app/Http/Controllers/DepositController.php
@@ -76,7 +76,7 @@ class DepositController extends Controller
                                 try {
                                     $depositHist = new deposits;
                                     $depositHist->user_id = $user->id;
-                                    $depositHist->deposit_id = ($user->id .'_'.$coupon->id .'_'. $bank->id); //User ID _ Coupon ID _ Bank ID for relational reasons
+                                    $depositHist->deposit_id = ($user->id .'_'. $coupon->id .'_'. $bank->id); //User ID _ Coupon ID _ Bank ID for relational reasons
                                     $depositHist->usn = ($user->firstname .' '. $user->lastname);
                                     $depositHist->amount = $request->input('amount');
                                     $depositHist->acct_name = "Account Name";
