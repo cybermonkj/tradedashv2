1efb19ff6d2e23941a56a04ee85d7d7e4a238c2a
Joshua Anop <rolexy.inc@gmail.com>
2021-02-07 13:48:38 +0100

    Added View Generator

--- core/app/Http/Controllers/exchange.php
+++ core/app/Http/Controllers/exchange.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class exchange extends Controller
 {
     //
+
+    public function show($id)
+    {
+        return view('user.exchange', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+
 }
