cee5725d97fd02c4585af9ecc66c4ffee9511318
Joshua Anop <rolexy.inc@gmail.com>
2021-02-07 14:54:27 +0100

    Corrected View

--- core/resources/views/user/trade.blade.php
+++ core/resources/views/user/trade.blade.php
@@ -2,13 +2,36 @@
 @extends('layouts.atlantis.layout')
 @Section('content')
             
+<div class="main-panel">
+    <div class="content">
+        @php($breadcome = 'Live Trader (Beta)')
+        @include('user.atlantis.main_bar')
+        <div class="page-inner mt--5">
+            @include('user.atlantis.overview')
+            <div id="prnt"></div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-head-row">
+ 
 <!-- MT5 Trader start -->
-            < <div class="col p-4" style="margin-left:-30px">
-                    <iframe src="https://trade.mql5.com/trade" name="WebTrader" title="Stock/Forex Trader" frameborder="0" style="display: block; border: none; height: 76vh; width: 80vw;"></iframe>
+
+                 < <div class="v-tour-object" style="margin-left:30" width="100%" scrolling="no">
+                    <iframe src="https://trade.mql5.com/trade" name="Trader" title="Stock/Forex Trader" frameborder="0" style="display: block; border: none; height: 76vh; width: 80vw;"></iframe>
                         </div>
                     </div>
                 </div> 
+
+                <script>
+                    if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+                      document.querySelector('.v-tour-object').style.display = "none"
+                   }
+                   </script>
+
 <!-- end of chart -->
+
+
 
 
 
