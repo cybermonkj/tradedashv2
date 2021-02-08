fe9b8eeec3bb031b16c15c56ab9c50c6ea97b49e
Joshua Anop <rolexy.inc@gmail.com>
2021-02-08 07:42:08 +0100

    Added Live view

--- core/resources/views/user/exchange.blade.php
+++ core/resources/views/user/exchange.blade.php
@@ -5,7 +5,7 @@
 
 <div class="main-panel">
     <div class="content">
-        @php($breadcome = 'Exchange (Live)')
+        @php($breadcome = 'Exhange (Demo)')
         @include('user.atlantis.main_bar')
         {{-- <div class="page-inner mt--5">
             @include('user.atlantis.overview')
@@ -13,18 +13,17 @@
             <div class="row">
                 <div class="col-md-12"> --}}
                     <div class="card">
-                        {{-- <div class="card-header">
+                        <div class="card-header">
                             <div class="card-head-row">
-                                <div class="card-title">{{ __('This Weeks Trade') }}</div>                                        --}}
+                                <div class="card-title">{{ __('This Weeks Trade') }}</div>                                       
                             </div>
                         </div>
-                        {{-- <div class="card-body">   --}}
-
-                                                <div class="mobileHide" style="margin-left:20" width="100%" scrolling="yes">
-                                                    <iframe src="https://dash.tradepander.com/exchange/" name="exhange" title="Exchanger" frameborder="0" style="display: block; border: none; height: auto; width: auto;"></iframe>
-                                                        </div>
-                                                    </div>
-                                                </div> 
+                                <div class="card-body">  
+                                        <div class="mobileHide" style="margin-left:20" width="60%" scrolling="no">
+                                            <iframe src="https://dash.tradepander.com/exchange/" name="Exchange" title="Exchange" frameborder="0" style="display: block; border: none; height: 76vh; width: 80vw;"></iframe>
+                                                </div>
+                                            </div>
+                                        </div> 
 
                                             </div>
                                         </div>
