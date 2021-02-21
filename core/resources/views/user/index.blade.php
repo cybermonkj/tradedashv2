5bd1010f265c18e3f41a9ff05ff3768e25fe7a8e
Snapster02 <alabson.inc@gmail.com>
2021-02-21 14:26:56 +0100

    Updated styles x4

--- core/resources/views/user/index.blade.php
+++ core/resources/views/user/index.blade.php
@@ -127,90 +127,92 @@
 									</div>
 								</div>
 								
-								<div class="card-body pb-0">									
+								<div class="pb-0 card-body">									
 									<div class="pull-in">
 										<canvas id="wd_stats"></canvas>
 									</div>
 								</div>
 							</div>
 							<div class="card">
-								<div class="card-body pb-0">
-									<div class="px-2 pb-2 pb-md-0 text-center">
+								<div class="pb-0 card-body">
+									<div class="px-2 pb-2 text-center pb-md-0">
 										<div id="circles-logs"></div>
-										<h6 class="fw-bold mt-3 mb-0">{{ __('Action History') }}</h6>
+										<h6 class="mt-3 mb-0 fw-bold">{{ __('Action History') }}</h6>
 										<br>
 									</div>									
 								</div>
 							</div>
 						</div>
 					</div>
-
-					<div class="row">
-						<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<div class="card-title">{{ __('Available Packages') }} </div>
-								</div>
-								<div class="card-body pb-0">
-									@include('user.inc.packages')
+					
+					<!--	Container	-->
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<div class="card-title">{{ __('Available Packages') }} </div>
+									</div>
+									<div class="pb-0 card-body">
+										@include('user.inc.packages')
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div class="row row-card-no-pd">
-						<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<div class="card-head-row card-tools-still-right">
-										<h4 class="card-title">{{ __('User Activities') }}</h4>
-										<div class="card-tools">
-											<button class="btn btn-icon btn-link btn-primary btn-xs"><span class="fa fa-angle-down"></span></button>
-											<button class="btn btn-icon btn-link btn-primary btn-xs btn-refresh-card"><span class="fa fa-sync-alt"></span></button>
-											<button class="btn btn-icon btn-link btn-primary btn-xs"><span class="fa fa-times"></span></button>
-										</div>
-									</div>
-									<p class="card-category">{{ __('All actions by you on tradepander.') }}</p>
-								</div>
-								<div class="card-body">
-									<div class="row">
-										<div class="col-md-12">
-											<div class="card">												
-												<div class="card-body">
-													<div class="table-responsive">
-														<table id="basic-datatables" class="display table table-striped 
-														table-hover" >
-															<thead>
-																<tr>
-																	<th>{{ __('Actions') }}</th>
-																	<th>{{ __('Date') }}</th>
-																</tr>
-															</thead>
-															<tfoot>
-																<tr>
-																	<th>{{ __('Actions') }}</th>
-																	<th>{{ __('Date') }}</th>
-																</tr>
-															</tfoot>
-															<tbody>																
-																@foreach( $logs as $log)
-																	<tr>
-																		<td>
-																			{{ $log->action }}
-																		</td>
-																		<td>{{ $log->created_at }}</td>
-																	</tr>
-																@endforeach																
-															</tbody>
-														</table>														
-													</div>
-													
-												</div>
+						<div class="row row-card-no-pd">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<div class="card-head-row card-tools-still-right">
+											<h4 class="card-title">{{ __('User Activities') }}</h4>
+											<div class="card-tools">
+												<button class="btn btn-icon btn-link btn-primary btn-xs"><span class="fa fa-angle-down"></span></button>
+												<button class="btn btn-icon btn-link btn-primary btn-xs btn-refresh-card"><span class="fa fa-sync-alt"></span></button>
+												<button class="btn btn-icon btn-link btn-primary btn-xs"><span class="fa fa-times"></span></button>
 											</div>
 										</div>
-										
+										<p class="card-category">{{ __('All actions by you on tradepander.') }}</p>
 									</div>
+									<div class="card-body">
+										<div class="row">
+											<div class="col-md-12">
+												<div class="card">												
+													<div class="card-body">
+														<div class="table-responsive">
+															<table id="basic-datatables" class="table display table-striped table-hover" >
+																<thead>
+																	<tr>
+																		<th>{{ __('Actions') }}</th>
+																		<th>{{ __('Date') }}</th>
+																	</tr>
+																</thead>
+																<tfoot>
+																	<tr>
+																		<th>{{ __('Actions') }}</th>
+																		<th>{{ __('Date') }}</th>
+																	</tr>
+																</tfoot>
+																<tbody>																
+																	@foreach( $logs as $log)
+																		<tr>
+																			<td>
+																				{{ $log->action }}
+																			</td>
+																			<td>{{ $log->created_at }}</td>
+																		</tr>
+																	@endforeach																
+																</tbody>
+															</table>														
+														</div>
+														
+													</div>
+												</div>
+											</div>
+											
+										</div>
 
+									</div>
 								</div>
 							</div>
 						</div>
