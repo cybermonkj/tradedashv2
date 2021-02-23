8835bb32f0e53c0c667dc960902d24dd3595c287
Snapster02 <alabson.inc@gmail.com>
2021-02-23 16:41:24 +0100

    changes x2

--- core/resources/views/user/atlantis/main_bar.blade.php
+++ core/resources/views/user/atlantis/main_bar.blade.php
@@ -1,12 +1,25 @@
 <style>
+	.custom-flex {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
 	#actions {
-		border: 2px solid red;
+		/* border: 2px solid red; */
+		display: flex;
+		align-items: center;
+	}
+
+	@media (min-width: 768px) {
+		#actions {
+			
+		}
 	}
 </style>
 
 <div class="panel-header " style="background-color: {{$settings->header_color}}">
 	<div class="py-5 page-inner" style="background-color: {{$settings->header_color}}">
-		<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+		<div class="custom-flex">
 			<div>
 				<h2 class="pb-2 text-white fw-bold">
 					{{ $breadcome }}
