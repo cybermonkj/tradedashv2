49adbc77314bf59a68acfd77f2e5daef625c1d21
Snapster02 <alabson.inc@gmail.com>
2021-02-23 21:36:38 +0100

    Updated code

--- core/resources/views/user/inc/mob_inv.blade.php
+++ core/resources/views/user/inc/mob_inv.blade.php
@@ -1,36 +1,4 @@
-                
-<div class="alert alert-info inv_alert_cont" >
-    <div class="row inv_alert_top_row">
-        <div class="col-xs-12 pad_top_5" align="center" >
-            <h4 class="u_case">{{ __('') }}Package: {{$in->package}}</h4>
-           
-        </div>
-    </div> 
-    <div class="row color_blue_9">
-        <div class="col-xs-6">
-            {{ __('Capital:') }}
-        </div>
-        <div class="col-xs-6">
-            {{($settings->currency)}} {{$in->capital}}
-        </div>
-    </div> 
-    <div class="row" style="">
-        <div class="col-xs-6">
-            {{ __('Return:') }}
-        </div>
-        <div class="col-xs-6">
-            {{($settings->currency)}} {{$in->i_return}}
-        </div>
-    </div>  
     {{-- <div class="row" style="">
-        <div class="col-xs-6">
-            {{ __('Started:') }}
-        </div>
-        <div class="col-xs-6">
-            {{$in->date_invested}}
-        </div>
-    </div>  --}}
-    <div class="row" style="">
         <div class="col-xs-6">
             {{ __('Ending:') }}
         </div>
@@ -46,14 +14,7 @@
             {{$totalDays}}
         </div>
     </div>
-    {{-- <div class="row" style="">
-        <div class="col-xs-6">
-           {{ __('Withdrawn:') }} 
-        </div>
-        <div class="col-xs-6">
-            {{($settings->currency)}} {{$in->w_amt}}
-        </div>
-    </div>  --}}
+
     <div class="row" style="">
         <div class="col-xs-6">
             {{ __('Status:') }}
@@ -71,5 +32,4 @@
         </div>
         {{ __('Click to withdraw') }}
     </div>                                                                     
-</div>
-        
+</div> --}}
