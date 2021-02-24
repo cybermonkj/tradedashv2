1fb8bd1562b86685eca3e9cdf37704e588fccdd8
Cyber Panel <kimtomato1999@gmail.com>
2021-02-24 12:02:10 +0000

    Auto commit by CyberPanel Daily cron on 02-24-2021_12-01-13

--- core/storage/framework/views/f2894da93971bc1bd310eb30ca4ff22ba85aad99.php
+++ core/storage/framework/views/f2894da93971bc1bd310eb30ca4ff22ba85aad99.php
@@ -1,14 +1,27 @@
+<style>
+	.custom-flex {
+		display: flex;
+		justify-content: space-between;
+		align-items: center !important;
+	}
+	#actions {
+		/* border: 2px solid red; */
+		display: flex;
+		align-items: center;
+	}
+</style>
+
 <div class="panel-header " style="background-color: <?php echo e($settings->header_color); ?>">
-	<div class="page-inner py-5" style="background-color: <?php echo e($settings->header_color); ?>">
-		<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+	<div class="py-5 page-inner" style="background-color: <?php echo e($settings->header_color); ?>">
+		<div class="custom-flex">
 			<div>
-				<h2 class="text-white pb-2 fw-bold">
+				<h2 class="pb-2 text-white fw-bold">
 					<?php echo e($breadcome); ?>
 
 				</h2>				
 			</div>
-			<div class="ml-md-auto py-2 py-md-0">
-				<a href="/<?php echo e($user->username); ?>/investments" class="btn btn-white btn-border btn-round mr-2">Investments</a>
+			<div class="py-2 ml-md-auto py-md-0" id="actions">
+				<a href="/<?php echo e($user->username); ?>/investments" class="mr-2 btn btn-white btn-border btn-round">Investments</a>
 				<a href="/<?php echo e($user->username); ?>/wallet" class="btn btn-secondary btn-round">Deposit</a>
 			</div>
 		</div>
