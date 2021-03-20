710bb2e53176ead089dfa5560d57fa503023697a
Alabs02 <alabson.inc@gmail.com>
2021-03-20 21:02:08 +0100

    Updated bootstrap app.php

--- core/bootstrap/app.php
+++ core/bootstrap/app.php
@@ -25,6 +25,10 @@ $app = new Illuminate\Foundation\Application(
 | incoming requests to this application from both the web and CLI.
 |
 */
+$app->bind('path.public', function ()
+{
+    return base_path() . '/public_html';
+});
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
