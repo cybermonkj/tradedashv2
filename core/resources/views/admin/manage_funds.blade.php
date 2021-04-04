83878cf0a538ab076cece0b6dfcd9e3c27378b77
Alabs02 <alabson.inc@gmail.com>
2021-04-04 13:42:46 +0100

    changes xx

--- core/resources/views/admin/manage_funds.blade.php
+++ core/resources/views/admin/manage_funds.blade.php
@@ -70,6 +70,7 @@
                                 </form>
                             </div>
                           </div>
+                        </div>
 
                     <!--    Modals End  -->
                     
