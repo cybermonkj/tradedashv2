552aab8f6f3f7c80dd404f170740d31bb92d6835
Alabs02 <alabson.inc@gmail.com>
2021-04-04 22:28:45 +0100

    Gradually fixing things x9

--- core/app/Http/Controllers/adminController.php
+++ core/app/Http/Controllers/adminController.php
@@ -124,6 +124,10 @@ class adminController extends Controller
       //   ->select('U.id', 'U.firstname', 'U.lastname', 'U.username', 'U.wallet', 'I.status', 'I.date_invested', 'I.date_end', 'I.package')
       //   ->where('U.wallet', '>', 'I.i_return')
       //   ->get();
+      $result = DB::table('users')
+        ->join('invest', 'users.id', '=', 'invest.user_id')
+        ->where('users.wallet', '>', 'invest.i_return')
+        ->get();
 
         return view('admin.manage_funds');
 
