d0a63a33903342113075c9d9077612ca18f5a86d
Alabs02 <alabson.inc@gmail.com>
2021-04-04 11:51:37 +0100

    new route

--- core/resources/views/admin/manage_funds.php
+++ core/resources/views/admin/manage_funds.php
@@ -1,8 +1,8 @@
 @extends('admin.atlantis.layout')
 @Section('content')
-    <div class="container">
-        <div class="content">
-            
+    <div class="content">
+        <div class="container">
+            <h1>Display</h1>
         </div>
     </div>
 @endSection
